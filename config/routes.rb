438512c6531c145d1f87334099fe26bc56dc87ca
EC2 Default User <usuikenta100@gmail.com>
2021-10-04 10:54:39 +0000

    [add]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
   resources :users, only: [:show, :index, :edit, :update]
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resource :book_comments, only: [:create, :destroy]
+  end
+
 end
