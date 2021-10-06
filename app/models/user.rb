62404d9fe3a1afe71e02bea1f96778a0decd6537
EC2 Default User <usuikenta100@gmail.com>
2021-10-06 12:31:34 +0000

    [start]

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,24 @@ class User < ApplicationRecord
   has_many :favorites
   has_many :book_comments
 
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id"
+  has_many :followers, through: :reverse_of_relations, source: :follower
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id"
+  has_many :followings, through: :relationships, source: :followed
+  
+  def follow(user_id)
+    relationships.create(follwed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
+  end
+
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users, only: [:show, :index, :edit, :update]
+  resources :users, only: [:show, :index, :edit, :update] do
+    resource :relationships, only[:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
