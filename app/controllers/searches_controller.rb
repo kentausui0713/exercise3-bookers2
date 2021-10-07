74dc01b99094e468101b6c264537210facf7542e
EC2 Default User <usuikenta100@gmail.com>
2021-10-07 15:51:45 +0000

    [Finish]Exercise5

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,37 @@
+class SearchesController < ApplicationController
+  def search
+    @model = params["model"]
+    @method = params["method"]
+    @content = params["content"]
+    @records = search_for(@model, @content, @method)
+  end
+
+  private
+  def search_for(model,content,method)
+    if model == 'user'
+      if method == 'perfect'
+        User.where(name: content)
+      elsif method == 'prefix'
+        User.where('name LIKE ?', content+'%')
+      elsif method == 'backword'
+        User.where('name LIKE ?', '%'+content)
+      else
+        User.where('name LIKE ?','%'+content+'%')
+      end
+    elsif model == 'book'
+      if method == 'perfect'
+        Book.where(title: content)
+      elsif method == 'prefix'
+        Book.where('title LIKE ?', content+'%')
+      elsif method == 'backword'
+        Book.where('title LIKE ?', '%'+content)
+      else
+        Book.where('title LIKE ?', '%'+content+'%')
+      end
+    end
+
+  end
+
+
+
+end
